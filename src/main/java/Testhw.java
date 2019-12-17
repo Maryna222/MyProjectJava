03f80a7932ef636b0c4df66f591afb8c94cbac23
Mary_n <mankomv77@gmail.com>
2019-12-17 22:53:55 +0300

    Second commit on test branch

--- src/main/java/Testhw.java
+++ src/main/java/Testhw.java
@@ -1,5 +1,5 @@
 public class Testhw {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World! and some new text for second commit");
     }
 }
